8b8a60f3e98edeb5514dd186aabc3354af96a858
Sanieca <masonsanieca7@gmail.com>
2024-10-05 21:00:38 +0800

    Initial commit

---
+++ student.py
@@ -0,0 +1,9 @@
+students = ['Ariston', 'Faith', 'David', 'Samantha']
+index = int(input("Enter the index: "))
+
+
+if 0 <= index < len(students):
+    print(students[index])
+else:
+    print("Index out of range")
+   
